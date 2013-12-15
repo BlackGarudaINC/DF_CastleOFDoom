967cfd5d1c90f5fa03878e2a8e81bb4c11b6b1e6
scott-weaver <scott.a.weaver@gmail.com>
2013-12-14 20:31:28 -0500

    Finished gargoyle

--- frankenstein/frankenstein/lib/game/entities/enemy/gargoyle.js
+++ frankenstein/frankenstein/lib/game/entities/enemy/gargoyle.js
@@ -2,7 +2,7 @@ ig.module(
 	'game.entities.enemy.gargoyle'
 )
 .requires(
-	'impact.entity',
+	'game.entities.player.stoneparticle',
 	'game.entities.enemy.enemy'
 )
 .defines(function(){
@@ -17,6 +17,8 @@ EntityGargoyle = EntityEnemy.extend({
 	size: {x: 27, y: 34},
 	offset: {x: 18, y: 30},
 	maxVel: {x: 200, y: 200},
+
+	offsetChange: 20,	// a different y-offset is used for flying vs. idle
 
 	knockback: false,
 	damageFlash: true,
@@ -37,6 +39,7 @@ EntityGargoyle = EntityEnemy.extend({
 	awake: false,		// Awake and moving around
 
 	attackTimer: null,
+	idleTimer: null,	// How long it has to remain idle before attacking again
 	attackSpeed: 100,
 	returnSpeed: 80,
 
@@ -44,11 +47,14 @@ EntityGargoyle = EntityEnemy.extend({
 	position2: {x: 0, y: 0},
 	target: {x: 0, y:0},
 
+	foundX: false,
+	foundY: false,
+
 	lastPos: 1,
 
 	animSheet: new ig.AnimationSheet( 'media/sprites/Gargoyle.png', 64, 64 ),
 
-	debugDraw: true,
+	// debugDraw: true,
 
 	init: function( x, y, settings ) {
 		this.parent( x, y, settings );
@@ -57,13 +63,16 @@ EntityGargoyle = EntityEnemy.extend({
 		this.addAnim( 'prepare', 0.3, [2,3] );
 		this.addAnim( 'fly', 0.1, [2,3] );
 		this.addAnim( 'attack', 1, [2] );
-		this.addAnim( 'death', 0.1, [4,5,6,7] );
+		this.addAnim( 'death', 0.2, [4,5,6,7], true );
 
 		// Define position 2 in weltmeister.
 		// If it's not defined, it will just return back to position 1.
 		this.position1.x = this.pos.x;
 		this.position1.y = this.pos.y;
+	},
 
+	startUpdate: function() {
+		this.parent();
 		this.becomeIdle();
 	},
 
@@ -77,6 +86,7 @@ EntityGargoyle = EntityEnemy.extend({
 		this.accel.y = 0;
 		this.awake = false;
 		this.returning = false;
+		this.idleTimer = new ig.Timer(3);
 	},
 
 	// Choose where to perch next
@@ -85,6 +95,7 @@ EntityGargoyle = EntityEnemy.extend({
 			this.lastPos = 2;
 			this.target = this.position2;
 		} else {
+			this.lastPos = 1;
 			this.target = this.position1;
 		}
 	},
@@ -102,6 +113,10 @@ EntityGargoyle = EntityEnemy.extend({
 				this.attackTimer = null;
 			}
 			
+		}
+
+		if (this.idleTimer != null && this.idleTimer.delta() > 0) {
+			this.idleTimer = null;
 		}
 
 		this.parent();
@@ -122,6 +137,9 @@ EntityGargoyle = EntityEnemy.extend({
 
 		this.accel.x = 0;
 		this.accel.y = 0;
+
+		this.foundX = false;
+		this.foundY = false;
 
 		this.flip = (this.pos.x < this.target.x);
 
@@ -150,23 +168,62 @@ EntityGargoyle = EntityEnemy.extend({
 	myUpdate: function() {
 
 		// If not awake yet, check if close enough to start waking up and preparing to attack
-		if (!this.awake && this.distanceTo(ig.game.player) < 150) {
+		if (!this.awake && this.idleTimer == null && this.distanceTo(ig.game.player) < 150) {
 			this.awake = true;
 			this.attackTimer = new ig.Timer(2);
 			this.accel.y = -10;
 			this.currentAnim = this.anims.prepare.rewind();
 			this.flip = (this.pos.x < ig.game.player.pos.x);
+			this.offset.y -= this.offsetChange;
 		}
 
 		// If returning to a perch, check if you've almost made it
-		if (this.awake && this.currentAnim == this.anims.fly && this.pos.x > this.target.x - 4 && this.pos.x < this.target.x + 4 && this.pos.y < this.target.x + 4 && this.pos.y > this.target.y - 4) {
-			this.becomeIdle();
+		if (this.awake && this.currentAnim == this.anims.fly) {
+			if (this.pos.x > this.target.x - 4 && this.pos.x < this.target.x + 4 && this.pos.y < this.target.y + 4 && this.pos.y > this.target.y - 4) { // Made it
+				this.offset.y += this.offsetChange;
+				this.pos.x = this.target.x;
+				this.pos.y = this.target.y;
+				this.becomeIdle();
+			} else if (this.pos.x > this.target.x - 4 && this.pos.x < this.target.x + 4 && !this.foundX) { // Matched x-coords
+				this.vel.x = 0;
+				if (!this.foundY) {
+					this.vel.y = (this.pos.y < this.target.y ? this.returnSpeed/2 : -this.returnSpeed/2);
+				}
+				this.foundX = true;
+				this.pos.x = this.target.x;
+			} else if (this.pos.y > this.target.y - 4 && this.pos.y < this.target.y + 4 && !this.foundY) { // Matched y-coords
+				this.vel.y = 0;
+				if (!this.foundX) {
+					this.vel.x = (this.pos.x < this.target.x ? this.returnSpeed/2 : -this.returnSpeed/2);
+				}
+				this.foundY = true;
+				this.pos.y = this.target.y;
+			}
 		}
 
 		this.currentAnim.flip.x = !this.flip;
 
 		this.parent();
 	},
+
+	deathCallback: function() {
+		this.parent();
+		this.vel.x = 0;
+		this.vel.y = 0;
+		this.accel.x = 0;
+		this.accel.y = -10;
+	},
+
+	kill: function() {
+		this.parent();
+
+		// Make stone particles spawn
+		for (var i=0; i<15; i++) {
+			ig.game.spawnEntity( EntityStoneparticle, this.pos.x + Math.random()*20, this.pos.y + Math.random()*20, {image: 1} );
+			ig.game.spawnEntity( EntityStoneparticle, this.pos.x + Math.random()*20, this.pos.y + Math.random()*20, {image: 2} );
+		}
+		
+	}
 
 });
 
